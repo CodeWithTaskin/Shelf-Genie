8ca49a06b540165f431167a491ad6890fc15a10f
Farjhan Ahmed <nkftt58346@gmail.com>
2025-07-02 11:48:59 +0600

    CI-CD Test-15

--- check_env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-mongo_url = os.getenv("MONGODB_URL")
-if mongo_url and mongo_url.startswith("mongodb+srv"):
-    print("✅ MongoDB URI is valid and starts with 'mongodb'")
-else:
-    print("❌ MongoDB URI is missing or invalid")
